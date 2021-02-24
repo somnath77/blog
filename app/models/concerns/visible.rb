1b5bde22cb1c4fd9a7c11f0b8f1184f9b35bd723
Somnath <Somnath.surwase@joshsoftware.com>
2021-02-24 17:15:33 +0530

    All crud operations are working properli

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -9,16 +9,14 @@ module Visible
     end
     
     class_methods do
-        def public_count
-          where(status: 'public').count
+        def public_count(id)
+          where(status: 'public',article_id:id).count
         end
     end
-    
+
     def archived?
       status == 'archived'
     end
-
     
-
   end
   
